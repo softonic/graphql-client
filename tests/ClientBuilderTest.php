11460db59be56f56792c7f71326371b8602e2f8b
Christopher Fuchs <christopher.fuchs@ready2order.com>
2018-02-07 10:45:10 +0100

    #6 Added possibility to provide custom guzzle options
    * added test for using ClientBuilder with guzzle options

--- tests/ClientBuilderTest.php
+++ tests/ClientBuilderTest.php
@@ -2,6 +2,7 @@
 
 namespace Softonic\GraphQL\Test;
 
+use GuzzleHttp\Cookie\CookieJar;
 use PHPUnit\Framework\TestCase;
 use Softonic\GraphQL\ClientBuilder;
 
@@ -10,6 +11,16 @@ class ClientBuilderTest extends TestCase
     public function testBuild()
     {
         $client = ClientBuilder::build('http://foo.bar/qux');
+        $this->assertInstanceOf(\Softonic\GraphQL\Client::class, $client);
+    }
+
+    public function testBuildWithGuzzleOptions()
+    {
+        $guzzleOptions = [
+            'cookies' => new CookieJar(),
+        ];
+
+        $client = ClientBuilder::build('http://foo.bar/qux', $guzzleOptions);
         $this->assertInstanceOf(\Softonic\GraphQL\Client::class, $client);
     }
 
@@ -27,6 +38,29 @@ class ClientBuilderTest extends TestCase
             $mockProvider,
             $mockTokenOptions,
             $mockCache
+        );
+        $this->assertInstanceOf(\Softonic\GraphQL\Client::class, $client);
+    }
+
+    public function testBuildWithOAuth2ProviderAndGuzzleOptions()
+    {
+        $mockCache = $this->createMock(\Psr\Cache\CacheItemPoolInterface::class);
+        $mockProvider = $this->createMock(\League\OAuth2\Client\Provider\AbstractProvider::class);
+        $mockTokenOptions = [
+            'grant_type' => 'client_credentials',
+            'scope' => 'myscope',
+        ];
+
+        $guzzleOptions = [
+            'cookies' => new CookieJar(),
+        ];
+
+        $client = ClientBuilder::buildWithOAuth2Provider(
+            'http://foo.bar/qux',
+            $mockProvider,
+            $mockTokenOptions,
+            $mockCache,
+            $guzzleOptions
         );
         $this->assertInstanceOf(\Softonic\GraphQL\Client::class, $client);
     }
